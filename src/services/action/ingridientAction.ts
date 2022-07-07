616f3cc64cb59664377087fb964868e6abe95703
vpprilipukhov <vpprilipukhov@gmail.com>
2022-07-08 02:37:59 +0300

    last hope

--- src/services/action/ingridientAction.ts
+++ src/services/action/ingridientAction.ts
@@ -1,47 +1,97 @@
-// import {createAsyncThunk} from "@reduxjs/toolkit";
 import axios from "axios";
-import {State} from "../../auxiliary/types/ingridientTypes"; //apiIngridient,
 import {AppDispatch} from "../setupStore";
+import {modalSlice, orderPostType} from "../reducer/modalReducer";
 import {ingridientSlice} from "../reducer/ingridientReducer";
-import {modalSlice} from "../reducer/modalReducer";
+import {State} from "../../auxiliary/types/ingridientTypes";
 
 
-// я не знаю как сделать проверку, как необходимо((
 export const fetchIngredient = () => async (dispatch: AppDispatch) => {
     try {
         dispatch(ingridientSlice.actions.ingridientFetching())
         const response = await axios.get<{ success: string, data: State[] }>('https://norma.nomoreparties.space/api/ingredients')
-        dispatch(ingridientSlice.actions.ingridientFetchingSuccess(response.data.data))
-    } catch (e) {
-        dispatch(ingridientSlice.actions.ingridientFetchingError(e.message))
-    }
 
+        if (response.data.success) {
+            dispatch(ingridientSlice.actions.ingridientFetchingSuccess(response.data.data))
+        } else {
+            dispatch(ingridientSlice.actions.ingridientFetchingError("Unable"))
+        }
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            dispatch(ingridientSlice.actions.ingridientFetchingError(error.message))
+        } else {
+            dispatch(ingridientSlice.actions.ingridientFetchingError("An unexpected error occurred"))
+        }
+
+    }
 }
 
-export const addOrderId = (id: Array<string>) => {
-    return (dispatch: AppDispatch) => {
-        axios
-            .post(
+
+export const addOrderId = (ingredients: Array<string>) => {
+    return async (dispatch: AppDispatch) => {
+        try {
+            const res = await axios.post<orderPostType>(
                 "https://norma.nomoreparties.space/api/orders",
                 JSON.stringify({
-                    ingredients: id
+                    ingredients,
                 }),
                 {
                     headers: {
                         "Content-Type": "application/json;charset=utf-8",
                     },
                 }
-            )
-            .then((response) => {
+            );
+
+            if (res.data.success) {
+                dispatch(modalSlice.actions.orderFetchingSuccess(res.data));
+            } else {
                 dispatch(
-                    modalSlice.actions.orderFetchingSuccess(response.data)
+                    modalSlice.actions.orderFetchingError("Unable to create order")
                 );
-            })
-            .catch((error) => {
+            }
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
                 dispatch(modalSlice.actions.orderFetchingError(error.message));
-            });
+            } else {
+                dispatch(
+                    modalSlice.actions.orderFetchingError("An unexpected error occurred")
+                );
+            }
+        }
     };
 }
+
+
+// }
+// const checkResponse = (res: any) => {
+//     return res.ok ? res.json() : res.json().then((err: any) => Promise.reject(err));
+// };
+//
+//
+// export const addOrderId = (id: Array<string>) => {
+//     return (dispatch: AppDispatch) => {
+//         axios
+//             .post(
+//                 "https://norma.nomoreparties.space/api/orders",
+//                 JSON.stringify({
+//                     ingredients: id
+//                 }),
+//                 {
+//                     headers: {
+//                         "Content-Type": "application/json;charset=utf-8",
+//                     },
+//                 }
+//             )
+//             .then(checkResponse)
+//             .then((response) => {
+//                 dispatch(
+//                     modalSlice.actions.orderFetchingSuccess(response.data)
+//                 );
+//             })
+//             .catch((error) => {
+//                 dispatch(modalSlice.actions.orderFetchingError(error.message));
+//             });
+//     };
+// }
 
 
 // export const fetchIngredient = createAsyncThunk(
